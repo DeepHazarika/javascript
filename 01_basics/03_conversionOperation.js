df974df5464d2a5c2b64471b7f86c6591fc38eca
DeepHazarika <97538143+DeepHazarika@users.noreply.github.com>
2023-09-20 07:45:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// "22" => 22
+// "22abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let boolenaIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(boolenaIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "xyz" => true
