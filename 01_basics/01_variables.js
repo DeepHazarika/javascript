e69e5b1c1887b10fdaae6a1791b641b7406a214a
DeepHazarika <97538143+DeepHazarika@users.noreply.github.com>
2023-09-20 07:31:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =  123456
+let accountEmail = "deep@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+// accountId = 2
+
+// console.log(accountId);
+
+accountEmail = 'peed@gmail.com'
+accountPassword = "178173"
+accountCity = "Guwahati"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);  
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node js not browser
+
+let name = "Peed"
+let age = 23
+let isLoggedIn = false
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
